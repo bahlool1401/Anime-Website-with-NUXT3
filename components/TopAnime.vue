f9df4cd900e85190fe438d49589432a9bb18ac8b
bahlool1401 <bahlool.ramesh@gmail.com>
2024-08-24 18:49:56 +0330

    add tailwind + fetch data and images from Api

---
+++ components/TopAnime.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="card-slider">
+            <!-- TopAnime::{{ TopAnime }} -->
+            <span v-for="(item, index) in onlyFirstFew" :key="index" class="border-b">
+            <!-- {{ item?.images?.webp }} -->
+             <img :src="item?.images?.webp?.image_url" class="lable" :alt="item?.title" @click="goAnimePage(item)"> 
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    TopAnime:{
+        type:Array,
+        required:true
+    }
+})
+// for DYNAMIC👇
+
+
+const { TopAnime } = toRefs(props)
+
+const onlyFirstFew = computed(() => {
+    if (!TopAnime.value) return
+    return TopAnime?.value.slice(0,9)
+})
+
+</script>
+
+<style  scoped>
+
+.card-slider {
+    margin: 60px auto;
+    text-align: center;
+}
+.lable {
+    position: realtive;
+    margin: 0 -35px;
+    display: inline-block;
+    width: 150px;
+    height: 200px;
+    border-radius: 5px;
+    box-shadow: 0 10px 6px -6px #777;
+    -webkit-transform: skew(-20deg) rotate(-20deg);
+    transform: skew(-20deg) rotate(-20deg);
+    -webkit-transition: .3s;
+    transition: .3s;
+}
+.lable:hover{
+    margin: 0 50px;
+    -webkit-transform: none;
+    transform: none;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="header">
+            <Navigation />
+        </div>
+        <div class=" max-w-6xl shadow-md m-auto min-h-screen pt-3">
+            <div class=" px-2">
+                <div class="topAnime">
+                    <div class=" h-10 w-full bg-green-900">
+
+                        <h1 class="text-2xl px-4 text-white font-bold">UpComing Anime:</h1>
+                    </div>
+                    <!-- <ClientOnly> -->
+
+                        <TopAnime :TopAnime="topAnime" />
+                    <!-- </ClientOnly> -->
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+onMounted(async () => {
+  await  getData()
+    console.log('beforeeeeeeeeee',topAnime.value);
+})
+
+
+const topAnime = ref([])
+
+
+async function getData() {
+    try {
+        // const { data, error } = await useFetch('https://api.jikan.moe/v4/seasons/now')
+        // if (data.value) {
+        //     response.value = data.value.data
+        // }
+
+        
+        const res=await useFetch('https://api.jikan.moe/v4/top/anime')
+        console.log('77777777777777777777',res.data);
+        topAnime.value=res.data.value?.data
+        
+        // if (result.value) {
+          //  console.log(result.value,result.value?.data);
+           // topAnime.value = result?.data || []
+        // }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+</script>
